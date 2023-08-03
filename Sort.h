2eefee1a6e0c980ac1c38106cd1ab7890a9808df
Beat0n <hujizheuestc@163.com>
2023-08-03 16:05:50 +0800

    mergeSort

--- Sort.h
+++ Sort.h
@@ -6,11 +6,11 @@
 #define ALGORITHM_SORT_H
 #include <vector>
 
-// O(n*n)
-std::vector<int> insertionSort(std::vector<int>& nums) {
+//! insertionSort:O(n*n)
+void insertionSort(std::vector<int>& nums) {
     for (int i=1;i<nums.size();i++) {
         if (nums[i] >= nums[i-1]) continue;
-        for (int j=i;j>=0;j--) {
+        for (int j=i;j>0;j--) {
             if (nums[j] < nums[j-1]) {
                 int tmp = nums[j];
                 nums[j] = nums[j-1];
@@ -20,12 +20,11 @@ std::vector<int> insertionSort(std::vector<int>& nums) {
             }
         }
     }
-    return nums;
 }
 
-// O(n*n)
-std::vector<int> selectionSort(std::vector<int>& nums) {
-    for (int i=0; i<nums.size(); i++) {
+//! selectionSort: O(n*n)
+void selectionSort(std::vector<int>& nums) {
+    for (int i=0; i<nums.size()-1; i++) {
         int min_index = i;
         for (int j=i+1; j<nums.size(); j++) {
             if (nums[j] < nums[min_index]) {
@@ -36,6 +35,42 @@ std::vector<int> selectionSort(std::vector<int>& nums) {
         nums[i] = nums[min_index];
         nums[min_index] = tmp;
     }
-    return nums;
+}
+
+//! mergeSort: O(n*lgn)
+// merge two parts of the divided vector
+void merge(std::vector<int>& nums, int start, int end) {
+    if (start == end) return;
+    int medium = (start + end) / 2;
+    std::vector<int> ans;
+    int i = start;
+    int j = medium + 1;
+    while (i <= medium && j <= end) {
+        if (nums[i] < nums[j]) {
+            ans.push_back(nums[i]);
+            i++;
+        } else {
+            ans.push_back(nums[j]);
+            j++;
+        }
+    }
+    if (i > medium) {
+        for (int k = j; k<=end; k++) {
+            ans.push_back(nums[k]);
+        }
+    } else {
+        for (int k = i; k<=medium; k++) {
+            ans.push_back(nums[k]);
+        }
+    }
+    for (int k = 0; k<ans.size(); k++) {
+        nums[start+k] = ans[k];
+    }
+}
+void mergeSort(std::vector<int>& nums, int start, int end) {
+    if (start == end) return;
+    mergeSort(nums, start, (start + end) / 2);
+    mergeSort(nums, ((start + end) / 2) + 1, end);
+    merge(nums, start, end);
 }
 #endif //ALGORITHM_SORT_H
--- main.cpp
+++ main.cpp
@@ -8,9 +8,19 @@ void printVector(std::vector<int> nums) {
 }
 
 int main() {
-    std::vector<int> nums{1,3,2,4,5,6,8,7};
-    printVector(insertionSort(nums));
+    std::vector<int> nums{5,6,2,3,8,6,1,12,54,16,2348,165};
+
+    insertionSort(nums);
+    printVector(nums);
+
     std::cout << std::endl;
-    printVector(selectionSort(nums));
+    nums = {5,6,2,3,8,6,1,12,54,16,2348,165};
+    selectionSort(nums);
+    printVector(nums);
+
+    std::cout << std::endl;
+    nums = {5,6,2,3,8,6,1,12,54,16,2348,165};
+    mergeSort(nums, 0, nums.size() - 1);
+    printVector(nums);
     return 0;
 }
